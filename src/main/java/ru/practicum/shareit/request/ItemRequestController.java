0added3f87e9353cffe4d6c733b677822ce92e4a
pEvgeniy <popevgeniyv@yandex.ru>
2023-09-07 18:53:54 +0300

    feat: @ResponseStatus()

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.request;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.service.ItemRequestService;
@@ -31,6 +33,7 @@ public class ItemRequestController {
     public static final String X_SHARER_USER_ID = "X-Sharer-User-Id";
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ItemRequestDto create(@Valid @RequestBody ItemRequestDto itemRequestDto,
                                  @RequestHeader(X_SHARER_USER_ID) int userId) {
         log.info("controller. post. /requests. create item request");
@@ -38,12 +41,14 @@ public class ItemRequestController {
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<ItemRequestDto> findAll(@RequestHeader(X_SHARER_USER_ID) int userId) {
         log.info("controller. get. /requests. find all item requests");
         return itemRequestService.findAllByRequester(userId);
     }
 
     @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
     public List<ItemRequestDto> findAllFromOthers(@RequestHeader(X_SHARER_USER_ID) int userId,
                                                   @RequestParam(defaultValue = "0") @Valid @PositiveOrZero int from,
                                                   @RequestParam(defaultValue = "10") @Valid @Positive int size) {
@@ -52,6 +57,7 @@ public class ItemRequestController {
     }
 
     @GetMapping("/{itemRequestId}")
+    @ResponseStatus(HttpStatus.OK)
     public ItemRequestDto findById(@PathVariable int itemRequestId,
                                    @RequestHeader(X_SHARER_USER_ID) int userId) {
         log.info("controller. get. /requests/{}. find item requests by id", itemRequestId);
